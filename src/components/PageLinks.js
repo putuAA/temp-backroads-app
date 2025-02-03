e43d2baae86d7642bd27641aced73143770df355
Putu Ardika Ardana <putu.ardika@photon.com>
2025-02-03 15:54:52 +0700

    first commit

---
+++ src/components/PageLinks.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { pageLinks } from "../data";
+import PageLink from "./PageLink";
+const PageLinks = ({ parentClass, itemClass }) => {
+  return (
+    <ul className={parentClass} id="nav-links">
+      {pageLinks.map((link) => {
+        return <PageLink link={link} itemClass={itemClass} />;
+      })}
+    </ul>
+  );
+};
+
+export default PageLinks;
